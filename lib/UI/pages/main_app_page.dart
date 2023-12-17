663dd0bda7075ccbcebe63aa65fe35c19f560f65
jeronimo Jimenez <sirfakelowell@gmail.com>
2023-12-17 16:09:39 -0500

    Primera solución de ordenamiento

--- lib/UI/pages/main_app_page.dart
+++ lib/UI/pages/main_app_page.dart
@@ -1,34 +1,68 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  void intercambiarPosiciones(
+      int indiceValorACambiar, int indice, List<int> resultado) {
+    final valorAnterior = resultado[indiceValorACambiar];
+    resultado[indiceValorACambiar] = resultado[indice];
+    resultado[indice] = valorAnterior;
+    // return resultado;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final DateTime dateTime = DateTime.now();
     const List<int> ejercicioUno = <int>[-2, 16, 2, 5, -6, 10];
-    List<int> resultado = <int>[...ejercicioUno];
-    int valorAnterior = 0;
-    int indice = 0;
-    for (int iteracion = 0; iteracion < resultado.length; iteracion++) {
-      if (valorAnterior > resultado[iteracion]) {
-        indice = iteracion;
-      }
+    //final List<int> ejercicioUno = generarNumerosAleatorios(100);
+    //final List<int> ejercicioUno = generarNumerosAleatorios(100);
+
+    final List<int> resultado = <int>[...ejercicioUno];
+    final List<Widget> infoWidget = <Widget>[];
+
+    void displayMsg(dynamic msg) {
+      infoWidget.add(
+        Text('(${DateTime.now().difference(dateTime).inMilliseconds}) => $msg'),
+      );
     }
-    valorAnterior = resultado[0];
-    resultado[0] = resultado[indice];
-    resultado[indice] = valorAnterior;
+
+    displayMsg('$dateTime => $ejercicioUno');
+
+    for (int index = 0; index < ejercicioUno.length; index++) {
+      int indice = index;
+
+      for (int iteracion = index + 1;
+          iteracion < resultado.length;
+          iteracion++) {
+        if (resultado[indice] > resultado[iteracion]) {
+          displayMsg('INTERCAMBIA');
+          indice = iteracion;
+        }
+      }
+      intercambiarPosiciones(index, indice, resultado);
+      displayMsg(resultado);
+    }
 
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Column(
-            children: [
-              Text('$ejercicioUno'),
-              Text('$resultado'),
-            ],
+          child: ListView.builder(
+            itemCount: infoWidget.length,
+            itemBuilder: (context, index) {
+              return infoWidget[index];
+            },
           ),
         ),
       ),
     );
   }
+}
+
+List<int> generarNumerosAleatorios(int lim) {
+  Random random = Random();
+  List<int> numeros = List.generate(lim, (_) => random.nextInt(20001) - 10000);
+  return numeros;
 }
