72b2b88fff222ebc06489842d8028027c5111754
Jocaagura <oficinameltablero@gmail.com>
2023-12-17 17:57:48 -0500

    refactor: Agregué una refactrizacion para separar la logica de negocios

--- lib/UI/pages/main_app_page.dart
+++ lib/UI/pages/main_app_page.dart
@@ -1,68 +1,40 @@
-import 'dart:math';
-
+import 'package:arreglos/business/controllers/main_app_controller.dart';
 import 'package:flutter/material.dart';
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({
+    super.key,
+    required this.mainAppController,
+  });
 
-  void intercambiarPosiciones(
-      int indiceValorACambiar, int indice, List<int> resultado) {
-    final valorAnterior = resultado[indiceValorACambiar];
-    resultado[indiceValorACambiar] = resultado[indice];
-    resultado[indice] = valorAnterior;
-    // return resultado;
-  }
+  final MainAppController mainAppController;
 
   @override
   Widget build(BuildContext context) {
-    final DateTime dateTime = DateTime.now();
-    const List<int> ejercicioUno = <int>[-2, 16, 2, 5, -6, 10];
-    //final List<int> ejercicioUno = generarNumerosAleatorios(100);
-    //final List<int> ejercicioUno = generarNumerosAleatorios(100);
-
-    final List<int> resultado = <int>[...ejercicioUno];
-    final List<Widget> infoWidget = <Widget>[];
-
-    void displayMsg(dynamic msg) {
-      infoWidget.add(
-        Text('(${DateTime.now().difference(dateTime).inMilliseconds}) => $msg'),
-      );
-    }
-
-    displayMsg('$dateTime => $ejercicioUno');
-
-    for (int index = 0; index < ejercicioUno.length; index++) {
-      int indice = index;
-
-      for (int iteracion = index + 1;
-          iteracion < resultado.length;
-          iteracion++) {
-        if (resultado[indice] > resultado[iteracion]) {
-          displayMsg('INTERCAMBIA');
-          indice = iteracion;
-        }
-      }
-      intercambiarPosiciones(index, indice, resultado);
-      displayMsg(resultado);
-    }
-
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: ListView.builder(
-            itemCount: infoWidget.length,
-            itemBuilder: (context, index) {
-              return infoWidget[index];
+          child: FutureBuilder<List<Widget>>(
+            future: mainAppController.generarlistaDeWidgets(8800),
+            builder: (_, AsyncSnapshot<List<Widget>> snapshot) {
+              final List<Widget>? children = snapshot.data;
+
+              if (children != null) {
+                if (children.isNotEmpty) {
+                  return ListView.builder(
+                    itemCount: children.length,
+                    itemBuilder: (context, index) {
+                      return children[index];
+                    },
+                  );
+                }
+              }
+
+              return const CircularProgressIndicator();
             },
           ),
         ),
       ),
     );
   }
-}
-
-List<int> generarNumerosAleatorios(int lim) {
-  Random random = Random();
-  List<int> numeros = List.generate(lim, (_) => random.nextInt(20001) - 10000);
-  return numeros;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:arreglos/UI/pages/main_app_page.dart';
+import 'package:arreglos/business/controllers/main_app_controller.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MainApp(
+    mainAppController: MainAppController(),
+  ));
 }
