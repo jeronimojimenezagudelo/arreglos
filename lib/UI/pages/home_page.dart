6ea069357aa22a987824c179619b645f32d3e28e
jeronimo Jimenez <sirfakelowell@gmail.com>
2023-12-28 18:12:34 -0500

    feat:se agrega animacion para la navegacion

--- lib/UI/pages/home_page.dart
+++ lib/UI/pages/home_page.dart
@@ -4,9 +4,9 @@ import 'package:arreglos/UI/pages/main_app_page.dart';
 import 'package:arreglos/business/controllers/main_app_controller.dart';
 import 'package:flutter/material.dart';
 
-const int cantidadIneficiente = 8800;
-const int cantidadEficiente = 400000;
-const int cantidadEficienteIsolate = 800000;
+const int cantidadIneficiente = 10;
+const int cantidadEficiente = 25;
+const int cantidadEficienteIsolate = 50;
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -44,6 +44,24 @@ class HomePage extends StatelessWidget {
   }
 }
 
+Route _createRoute(Widget page) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => page,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(0.0, 1.0);
+      const end = Offset.zero;
+      const curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
+
 class _ListTile extends StatelessWidget {
   const _ListTile({
     required this.page,
@@ -55,7 +73,10 @@ class _ListTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => page));
+        Navigator.push(
+          context,
+          _createRoute(page),
+        );
       },
       title: Text(title),
       leading: const Icon(Icons.arrow_forward_ios_rounded),
